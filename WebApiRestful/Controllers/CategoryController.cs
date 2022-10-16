05de1ae56c21a3ec4f7b0693f9780049fd1210c8
JinLong <phachco@gmail.com>
2022-10-16 14:48:17 +0700

    add file Category

---
+++ WebApiRestful/Controllers/CategoryController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sample.WebApiRestful.Service;
+using System.Threading.Tasks;
+
+namespace WebApiRestful.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCategory()
+        {
+            return Ok(await _categoryService.GetCategoryAll());
+        }
+
+        [HttpPatch]
+        public async Task<IActionResult> UpdateStatusAsync(int id)
+        {
+            return Ok(await _categoryService.UpdateStatus(id));
+        }
+
+        [HttpGet("get-name-category-by-id")]
+        public async Task<IActionResult> GetCategoryNameByIdAsync(int id)
+        {
+            IActionResult actionResult = null;
+            await Task.Factory.StartNew(() =>
+            {
+                actionResult = Ok(_categoryService.GetCategoryNameById(id));
+            });
+
+            return actionResult;
+        }
+    }
+
+
+
+}
