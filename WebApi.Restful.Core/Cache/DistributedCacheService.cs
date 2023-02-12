819acd2913e80c3bcb3c9088ba5aaa8785da94dd
JinLong <phachco@gmail.com>
2023-02-12 13:52:36 +0700

    missing files

---
+++ WebApi.Restful.Core/Cache/DistributedCacheService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+
+namespace WebApi.Restful.Core.Cache
+{
+    public class DistributedCacheService : IDistributedCacheService
+    {
+        private readonly IDistributedCache _cache;
+
+        public DistributedCacheService(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        public async Task Set<T>(string key, T value)
+        {
+            await _cache.SetStringAsync(key, JsonConvert.SerializeObject(value), new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(3),
+                SlidingExpiration = TimeSpan.FromMinutes(20)
+            });
+        }
+
+        public async Task<T> Get<T>(string key)
+        {
+            string data = await _cache.GetStringAsync(key);
+
+            if (!string.IsNullOrEmpty(data))
+            {
+                return JsonConvert.DeserializeObject<T>(data);
+            }
+
+            return default;
+        }
+
+        public async Task Remove(string key)
+        {
+            await _cache.RemoveAsync(key);
+        }
+    }
+}
+
---
+++ WebApi.Restful.Core/Cache/IDistributedCacheService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace WebApi.Restful.Core.Cache
+{
+    public interface IDistributedCacheService
+    {
+        Task<T> Get<T>(string key);
+        Task Remove(string key);
+        Task Set<T>(string key, T value);
+    }
+}
